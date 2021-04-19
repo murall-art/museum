6b3aebe94755514e57b606ab0b1120070e65b7d0
Toomas Oosalu <toomas.oosalu@gmail.com>
2021-04-20 00:38:48 +0400

    feat: fetching token metadata

---
+++ src/metadata/fetch.ts
@@ -0,0 +1,48 @@
+import { Contract } from 'web3-eth-contract'
+import { hexToAscii } from 'web3-utils'
+import { L1, L2 } from '../connections/web3'
+import save from './save'
+
+export type MetadataOptions = {
+  readonly isLayer2?: boolean
+  readonly save?: boolean
+  readonly downloadDir?: string
+}
+
+export type Metadata = {
+  name: string
+  artist: string
+  number: number
+  series: number
+  viewUrl: string
+  isFilled: boolean
+}
+
+export default async (id: number, opts?: MetadataOptions): Promise<Metadata | undefined> => {
+  if (id == null) return
+  const options = opts || {}
+  
+  const web3 = options.isLayer2 ? L2() : L1()
+  const nftContract = await web3.contracts.Nft.get()
+  
+  const fetchName = async (contract: Contract, tokenId: number): Promise<string> => {
+    const callSig = contract.methods.getName(tokenId).encodeABI()
+    const result = await web3.eth.call({ to: contract.options.address, data: callSig })
+    return hexToAscii(result).replace(/[\u0000-\u001F]/g, '').trim()
+  }
+
+  const [name, artist, number, series, viewUrl, isFilled] = await Promise.all([
+    fetchName(nftContract, id),
+    nftContract.methods.getArtist(id).call(),
+    nftContract.methods.getNumber(id).call(),
+    nftContract.methods.getSeriesId(id).call(),
+    nftContract.methods.viewURI(id).call(),
+    nftContract.methods.isArtworkFilled(id).call()
+  ])
+
+  const metadata: Metadata = { name, artist, number, series, viewUrl, isFilled }
+  if (options.save) {
+    await save(metadata, { directory: options.downloadDir, filename: `metadata_${id}.json` })
+  }
+  return metadata
+}
---
+++ src/metadata/save.ts
@@ -0,0 +1,20 @@
+import fs from 'fs'
+import { Metadata } from './fetch'
+
+export type FileOptions = {
+  directory?: string
+  filename?: string
+}
+
+export default async (metadata: Metadata, options?: FileOptions): Promise<void> => {
+  if (!metadata) return
+
+  const { directory, filename } = options || {}
+  const dir = directory || '.'
+  const name = filename || 'metadata.json'
+  const path = `${dir}/${name}`
+
+  fs.writeFile(path, JSON.stringify(metadata), (err) => {
+    if (err) console.error(err)
+  })
+}
