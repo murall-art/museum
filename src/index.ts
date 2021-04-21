c91b9083653f02f5f6086d09c03d0e293c54d86e
Toomas Oosalu <toomas.oosalu@gmail.com>
2021-04-21 23:55:47 +0400

    feat: expose metadata fetching

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 import { get as fetchToken, FetchTokenOptions } from './tokens/token'
+import { get as fetchMetadata, MetadataOptions as FetchMetadataOptions } from './tokens/metadata'
 
-export { fetchToken, FetchTokenOptions }
+export { fetchToken, fetchMetadata, FetchTokenOptions, FetchMetadataOptions }
